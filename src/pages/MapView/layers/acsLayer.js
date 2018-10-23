1eefd235579acafa7c8eae2a62fac890cedc5897
paultomchik <pauljtomchik@gmail.com>
2018-10-23 13:53:11 -0400

    ACS Layers

--- src/pages/MapView/layers/acsLayer.js
+++ src/pages/MapView/layers/acsLayer.js
@@ -2,6 +2,10 @@ const HOST = 'https://tig.nymtc.org/'
 const npmrdsLayer = {
 	name: 'Census ACS',
 	mapBoxSources: {
+    nymtc_areas: {
+    		type: 'vector',
+    		url: 'mapbox://am3081.32hog1ls'
+    },
   	},
   	type: 'Road Lines',
 	mapBoxLayers: [
@@ -35,9 +39,41 @@ function addLayers (map)  {
   	map.addSource(source, npmrdsLayer.mapBoxSources[source])
   })
 
-  npmrdsLayer.mapBoxLayers.forEach(layer => {
-  		map.addLayer(layer, 'waterway-label');
-  })
+    map.addLayer({
+        'id': 'counties',
+        'source': 'nymtc_areas',
+        'source-layer': 'counties',
+        'maxzoom': 10,
+        'type': 'fill',
+        'paint': {
+            'fill-color': 'rgba(0,0,196,0.25)',
+            // 'fill-opacity': 0.05
+        }
+    }, 'waterway-label');
+
+    map.addLayer({
+        'id': 'census_tracts',
+        'source': 'nymtc_areas',
+        'source-layer': 'census_tracts',
+        'minzoom': 10,
+        'maxzoom': 18,
+        'type': 'fill',
+        'paint': {
+            'fill-color': 'rgba(0,0,196,0.25)',
+        }
+    }, 'waterway-label');
+
+    // map.addLayer({
+        // 'id': 'tazs',
+        // 'source': 'nymtc_areas',
+        // 'source-layer': 'tazs',
+        // 'minzoom': 18,
+        // 'type': 'fill',
+        // 'paint': {
+            // 'fill-color': 'rgba(0,0,196,0.25)',
+        // }
+    // }, 'waterway-label');
+
 }
 
 export default npmrdsLayer;
