9c1e83a55cd5fc66db4cf3c48e5531da15105806
alex <am3081@gmail.com>
2019-01-10 16:43:11 -0500

    small changes

--- src/pages/MapView/layers/index.js
+++ src/pages/MapView/layers/index.js
@@ -3,14 +3,11 @@ import bpmLayer from './bpmLayer'
 import hubLayer from './hubBoundLayer'
 import tipLayer from './tipLayer'
 import acsLayer from './acsLayer'
+import buildings from './3d-buildings'
 
 import parcelLayer from './parcelLayer'
 
 export default {
-	npmrdsLayer,
-	//bpmLayer,
-	hubLayer,
-	tipLayer,
-	acsLayer,
-	parcelLayer
+	parcelLayer,
+	buildings
 }
